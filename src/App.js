99e03c289c839953a924bd0ebbd27db77d75a4f7
Jacky <chongkinye@gmail.com>
2023-08-04 15:58:38 -0400

    added a common components - Messages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Messages } from './components/common/Messages.jsx';
+
 function App() {
   return (
     <div className="App">
+      <Messages />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
