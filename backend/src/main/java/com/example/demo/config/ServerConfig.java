1a84b5eaa60b41aac25b10182d1343b43d9dacd3
Jacky <chongkinye@gmail.com>
2023-12-13 23:30:34 -0500

    jenkinsfile added and frontend server location will be configured from Jenkins

--- backend/src/main/java/com/example/demo/config/ServerConfig.java
+++ backend/src/main/java/com/example/demo/config/ServerConfig.java
@@ -2,4 +2,5 @@ package com.example.demo.config;
 
 public class ServerConfig {
     public final static String crossOrigin = "http://ckyjacky.ca:5001";
+    // public final static String crossOrigin = "http://localhost:5001";
 }
