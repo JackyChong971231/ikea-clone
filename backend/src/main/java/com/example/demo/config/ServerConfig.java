db9061fb6ec3aaeb89519a83c901f301df767e34
Jacky <chongkinye@gmail.com>
2023-12-18 12:12:10 -0500

    try allow ckyjacky.ca instead of specifiying the port

--- backend/src/main/java/com/example/demo/config/ServerConfig.java
+++ backend/src/main/java/com/example/demo/config/ServerConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.config;
 
 public class ServerConfig {
-    public final static String crossOrigin = "http://ckyjacky.ca:5001";
+    public final static String crossOrigin = "http://ckyjacky.ca";
     // public final static String crossOrigin = "http://localhost:5001";
 }
