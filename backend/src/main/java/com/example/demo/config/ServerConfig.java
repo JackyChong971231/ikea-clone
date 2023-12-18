706543a5125afcddc571e404858eebc300fb7014
Jacky <chongkinye@gmail.com>
2023-12-18 15:47:03 -0500

    remove port 5000 from cross origin and remove debug lines

--- backend/src/main/java/com/example/demo/config/ServerConfig.java
+++ backend/src/main/java/com/example/demo/config/ServerConfig.java
@@ -2,7 +2,7 @@ package com.example.demo.config;
 
 public class ServerConfig {
     public final static String ikeaCloneCrossOrigin = "http://ckyjacky.ca:5001";
-    public final static String personalWebsiteCrossOrigin = "http://ckyjacky.ca:5000";
+    public final static String personalWebsiteCrossOrigin = "http://ckyjacky.ca";
 //    public final static String ikeaCloneCrossOrigin = "http://localhost:5001"; // 5001 for ikea-clone and 3000 for personal website
 //    public final static String personalWebsiteCrossOrigin = "http://localhost:3000";
 }
