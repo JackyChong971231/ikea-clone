fdfa17086b5f5d4b1055f83478de110edf3ab0cd
Jacky <chongkinye@gmail.com>
2024-05-22 00:07:05 -0400

    server url and website url changed to https

--- backend/src/main/java/com/example/demo/config/ServerConfig.java
+++ backend/src/main/java/com/example/demo/config/ServerConfig.java
@@ -1,8 +1,8 @@
 package com.example.demo.config;
 
 public class ServerConfig {
-    public final static String ikeaCloneCrossOrigin = "http://ckyjacky.ca:5001";
-    public final static String personalWebsiteCrossOrigin = "http://ckyjacky.ca";
-//    public final static String ikeaCloneCrossOrigin = "http://localhost:5001"; // 5001 for ikea-clone and 3000 for personal website
+   public final static String ikeaCloneCrossOrigin = "https://ckyjacky.ca/ikea-clone";
+    public final static String personalWebsiteCrossOrigin = "https://ckyjacky.ca";
+    // public final static String ikeaCloneCrossOrigin = "http://localhost:5001"; // 5001 for ikea-clone and 3000 for personal website
 //    public final static String personalWebsiteCrossOrigin = "http://localhost:3000";
 }
